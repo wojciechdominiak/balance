90ee0614d15d792b87a87639bec2ee080f3ec78b
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-08-07 11:15:52 +0200

    change outcomeitem layout

--- src/components/outcomes/OutcomeItem.tsx
+++ src/components/outcomes/OutcomeItem.tsx
@@ -2,11 +2,16 @@ import React from "react";
 
 type MyProps = {
   name: string;
+  data: Date;
+  price: number;
   onRemoveOutcome: () => void;
 };
 
 export default class OutcomeItem extends React.Component<MyProps> {
   render() {
-    return <li onClick={this.props.onRemoveOutcome}>{this.props.name}</li>;
+    const content = `${this.props.data.toISOString()} ${this.props.name} ${
+      this.props.price
+    } `;
+    return <li onClick={this.props.onRemoveOutcome}>{content}</li>;
   }
 }
